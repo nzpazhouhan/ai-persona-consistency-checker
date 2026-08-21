b200a2d35e41dba53e7974f0e2c4bd9d380b43c9
nzpazhouhan <nz.pazhouhan@gmail.com>
2026-08-21 13:29:32 +0330

    return sum(scores) / len(scores)

--- app/evaluator.py
+++ app/evaluator.py
@@ -9,15 +9,19 @@ def evaluate(reference, answer):
     if not reference:
         return None
 
-    reference_text = ", ".join(reference)
-
-    reference_embedding = model.encode(reference_text)
-
     answer_embedding = model.encode(answer)
 
-    similarity = util.cos_sim(
-        reference_embedding,
-        answer_embedding
-    )
+    scores = []
 
-    return similarity.item()
+    for item in reference:
+
+        reference_embedding = model.encode(item)
+
+        similarity = util.cos_sim(
+            reference_embedding,
+            answer_embedding
+        )
+
+        scores.append(similarity.item())
+
+    return sum(scores) / len(scores)
