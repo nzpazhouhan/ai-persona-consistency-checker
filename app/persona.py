4c70abb909a0cb7c611cd25650fad446246fe48b
nzpazhouhan <nz.pazhouhan@gmail.com>
2026-08-30 14:42:09 +0330

    Changing Persona.py based on Characters new Schema

--- app/persona.py
+++ app/persona.py
@@ -4,21 +4,92 @@ import json
 class Persona:
     def __init__(self, data):
 
-        self.name = data["name"]
+        # Identity
+        self.name = data["identity"]["name"]
+        self.identity = data["identity"]["description"]
+        self.background = data["identity"].get("background", "")
 
-        self.identity = data["description"]
+        # Personality
+        personality = data.get("personality", {})
 
-        self.traits = data["traits"]
+        self.traits = personality.get("traits", [])
 
-        self.values = data["values"]
+        self.emotional_patterns = personality.get(
+            "emotional_patterns", {}
+        )
 
-        self.speech_style = data["speaking_style"]
+        self.cognitive_style = personality.get(
+            "cognitive_style", {}
+        )
 
-        self.likes = data.get("likes", [])
+        self.social_style = personality.get(
+            "social_style", {}
+        )
 
-        self.dislikes = data.get("dislikes", [])
+        self.behavioral_patterns = personality.get(
+            "behavioral_patterns", []
+        )
 
-        self.backstory = data.get("backstory", "")
+        # Values
+        values = data.get("values", {})
+
+        self.values = values.get("core_values", [])
+
+        self.moral_principles = values.get(
+            "moral_principles", []
+        )
+
+        self.beliefs = values.get(
+            "beliefs", []
+        )
+
+        self.boundaries = values.get(
+            "boundaries", []
+        )
+
+        # Motivations
+        motivations = data.get("motivations", {})
+
+        self.goals = motivations.get("goals", [])
+        self.drives = motivations.get("drives", [])
+        self.ambitions = motivations.get("ambitions", [])
+
+        # Preferences
+        preferences = data.get("preferences", {})
+
+        self.likes = preferences.get("likes", [])
+        self.dislikes = preferences.get("dislikes", [])
+        self.interests = preferences.get("interests", [])
+        self.hobbies = preferences.get("hobbies", [])
+
+        # Relationships
+        self.relationships = data.get("relationships", {})
+
+        # Decision making
+        self.decision_making = data.get(
+            "decision_making", {}
+        )
+
+        # Communication
+        self.speech_style = data.get(
+            "communication", {}
+        )
+
+        # Knowledge
+        self.knowledge = data.get(
+            "knowledge", {}
+        )
+
+        # Narrative
+        narrative = data.get("narrative", {})
+
+        self.backstory = narrative.get(
+            "backstory", ""
+        )
+
+        self.important_events = narrative.get(
+            "important_events", []
+        )
 
 
 def load_persona(path):
